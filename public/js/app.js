bbb222cc3aa883f1afd4b78985de0fb7523301a3
Bruno149 <Bruno147@live.com.pt>
2020-04-12 01:06:03 +0100

    Made some changes on DNS

--- public/js/app.js
+++ public/js/app.js
@@ -58231,8 +58231,8 @@ vue__WEBPACK_IMPORTED_MODULE_0___default.a.use(vuex__WEBPACK_IMPORTED_MODULE_1__
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\OpenStack\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\OpenStack\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\LTI_OpenStack\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\LTI_OpenStack\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/js/components/dns.vue
+++ resources/js/components/dns.vue
@@ -12,6 +12,37 @@ export default {
     };
   },
   methods: {
+
+     createDNS: function() {
+      let payload = {
+          name: this.dnsData.name,
+          email: this.dnsData.email
+  
+      };
+      console.log(payload);
+      axios
+        .post(
+          this.$store.state.url +
+            "/volume/v3/" +
+            this.$store.state.currentProjectID +
+            "/volumes",
+          payload,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "X-Auth-Token": this.$store.state.projectScopedToken
+            }
+          }
+        )
+        .then(response => {
+          if(response.status == 202){
+            Vue.$toast.open(
+              "Volume " + this.volumeData.name + " criado com sucesso!"
+            );
+          }
+        });
+    }
+  },
     
   },
   mounted() {
